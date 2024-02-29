7150172a540a5c305ad50a015425ef48c29a9411
Li BoSEN <59873116+PainterXIII@users.noreply.github.com>
2024-02-29 00:16:59 +0000

    修复上传失败

--- steam/utils/pan.py
+++ steam/utils/pan.py
@@ -55,7 +55,11 @@ def create_folder(folder_name, parentID, token):
 
 
 # 上传文件
-def upload_file(client_id, client_secret, parent, folder_name, file_path,flag):
+def upload_file(client_id, client_secret, folder_name, file_path,flag,getKey):
+    if getKey:
+        parent = 4134532
+    else:
+        parent = 4134530
     token = ''
     try:
         res_data = open_request('/api/v1/access_token', {'clientID': client_id, 'clientSecret': client_secret}, token)
@@ -64,21 +68,33 @@ def upload_file(client_id, client_secret, parent, folder_name, file_path,flag):
         try:
             if flag:
                 create_folder(folder_name, parent, token)
+            else:
+                logging.info(f"无需创建 {folder_name}")
         except:
-            logging.info(f"无需创建 {folder_name}")
-        filename = os.path.basename(file_path)
+            logging.info(f"文件创建失败 {folder_name}")
+        filename = os.path.basename(file_path).replace("_cache","")
         file_size = os.path.getsize(file_path)
-        with open(file_path, 'rb') as file:
-            file_etag = hashlib.md5(file.read()).hexdigest()
-        res_data = open_request('/upload/v1/file/create', {
-            'parentFileID': dirID,
-            'filename': filename,
-            'etag': file_etag,
-            'size': file_size
-        }, token)
+        if getKey:
+            with open(file_path, 'rb') as file:
+                file_etag = hashlib.md5(file.read()).hexdigest()
+            res_data = open_request('/upload/v1/file/create', {
+                'parentFileID': 4134532,
+                'filename': filename,
+                'etag': file_etag,
+                'size': file_size
+            }, token)
+        else:
+            with open(file_path, 'rb') as file:
+                file_etag = hashlib.md5(file.read()).hexdigest()
+            res_data = open_request('/upload/v1/file/create', {
+                'parentFileID': dirID,
+                'filename': filename,
+                'etag': file_etag,
+                'size': file_size
+            }, token)
 
         if res_data.get('reuse'):
-            logging.info("极速上传成功")
+            logging.info(f"极速上传成功 {filename} - {parent}")
             return
 
         upload_id = res_data['preuploadID']
@@ -101,7 +117,7 @@ def upload_file(client_id, client_secret, parent, folder_name, file_path,flag):
 
         res_data = open_request('/upload/v1/file/upload_complete', {'preuploadID': upload_id}, token)
         if res_data.get('completed'):
-            logging.info("上传成功")
+            logging.info(f"上传成功 {filename} - {parent}")
             return
 
         for j in range(200):
@@ -109,20 +125,20 @@ def upload_file(client_id, client_secret, parent, folder_name, file_path,flag):
             res_data = open_request('/upload/v1/file/upload_async_result',
                                     {'parentFileID': parent}, token)
             if res_data.get('completed'):
-                logging.info("上传成功")
+                logging.info(f"上传成功 {filename} - {parent}")
                 return
 
         logging.info("上传超时")
 
     except Exception as e:
-        logging.info(f"上传失败: {e}")
+        logging.info(f"上传失败: {e} - {file_path}")
 
 
-client_id = 'a7a437c9b93e4667b7fdf46ef7e912cb'  # 用户申请到的clientID
-client_secret = '6c09be6b4ef44406808eba1abd0d6222'  # 用户申请到的clientSecret
+client_id = 'a7a437c9b93e4667b7fdf46ef7e912cb'
+client_secret = '6c09be6b4ef44406808eba1abd0d6222' 
 #parent_file_id = 4119655  # 上传到的父级目录id，根目录为0
-#local_file_path = 'C:\\Users\\44615\\PycharmProjects\\Demo_Pan\\data\\depots\\1000360\\1000360.txt'  # 用户本地的文件绝对路径
+#local_file_path = 'C:\\Users\\44615\\PycharmProjects\\Demo_Pan\\data\\depots\\1000360\\1000360.txt'
 
 
-def upload_file_pan(app_id,file_path,parent_file_id,flag):
-    upload_file(client_id, client_secret, parent_file_id, app_id, file_path,flag)
+def upload_file_pan(app_id,file_path,flag,getKey):
+    upload_file(client_id, client_secret, app_id, file_path,flag,getKey)
